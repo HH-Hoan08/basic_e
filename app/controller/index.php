08f6062cc9350fdc4e139886141cd5b2ddcd28d2
HH-Hoan08 <Naohyuh1@gmail.com>
2026-06-03 15:23:08 +0700

    fix: path /basic_e

--- app/controller/index.php
+++ app/controller/index.php
@@ -1,6 +1,7 @@
 <?php
 $page = isset($_GET['page']) ? $_GET['page'] : 'home';
-define('BASE_URL', 'http://localhost/Web_NC/basic_e/');
+define('BASE_URL', 'http://localhost/basic_e/');
+
 include '../view/inc/header.php';
 
 switch ($page) {
