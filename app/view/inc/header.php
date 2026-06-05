792419cc264f6428ee5359c7bf10a7f034954cfa
HH-Hoan08 <Naohyuh1@gmail.com>
2026-06-05 12:16:59 +0700

    fix: line 98 icon bar

--- app/view/inc/header.php
+++ app/view/inc/header.php
@@ -93,8 +93,9 @@
 
                     <div class="dropdown">
                         <a class="nav-icon position-relative text-decoration-none dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            < i class="fa fa-fw fa-user text-dark mr-3"></i>
-                            <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">+99</span>
+                            <i class="fa fa-fw fa-user text-dark mr-3"></i>
+                            <!-- hiện thông báo mới của người dùng (cập nhật sau) -->
+                            <!-- <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">+99</span> -->
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
                             
